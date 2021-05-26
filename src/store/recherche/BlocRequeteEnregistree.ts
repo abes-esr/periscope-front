2c68620db6679ce9953825c05f1122144c652e32
Fabien Rétif <retif@abes.fr>
2021-05-26 15:52:02 +0200

    Fix: TypeError: Cannot read property 'criteres' of undefined at isSelectionEmpty

--- src/store/recherche/BlocRequeteEnregistree.ts
+++ src/store/recherche/BlocRequeteEnregistree.ts
@@ -1,6 +1,6 @@
 import {JsonGlobalSearchRequest} from '@/store/api/periscope/JsonInterfaces';
 
 export class BlocRequeteEnregistree {
-   _directRequest: JsonGlobalSearchRequest;
+   _directRequest: JsonGlobalSearchRequest = {criteres: [], tri: [], facettes: []};
    _historyOfAllRequests: Array<JsonGlobalSearchRequest> = [];
 }
